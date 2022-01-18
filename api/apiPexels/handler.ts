c8687dd86b9431c490e1cf073ba45fbff0266ba3
vlasta_stelmakh <vlasta.stelmakh2.0@gmail.com>
2022-01-18 17:40:58 +0300

    feat: get array buffer from url image

--- api/apiPexels/handler.ts
+++ api/apiPexels/handler.ts
@@ -8,6 +8,11 @@ import { Response } from "api/auth/auth.interfaces";
 import { PexelsManager } from "./apiPexels.manager";
 import { PexelsService } from "./apiPexels.service";
 import * as multipartParser from "lambda-multipart-parser";
+import * as axios from "axios";
+import { S3Service } from "../../services/s3.service";
+import * as sharp from "sharp";
+
+const S3 = new S3Service();
 
 export const getPexelsPhotos: APIGatewayProxyHandlerV2<Response> = async (
   event
@@ -28,18 +33,30 @@ export const getPexelsPhotos: APIGatewayProxyHandlerV2<Response> = async (
 export const postPexelsPhotos: APIGatewayProxyHandlerV2 = async (event) => {
   const client = createClient(getEnv("PEXELS_API_KEY"));
   const ids = JSON.parse(event.body!).ids;
-  log(ids);
+  // log(ids);
   const photos = await Promise.all(
     ids.map(async (id) => {
       return await client.photos.show({ id: id });
     })
   );
 
-  log("photos from array: ");
-  log(photos);
+  // log("photos from array: ");
+  // log(photos);
 
   /// upload to s3 image
+  //@ts-ignore
 
+  const image = await axios.get(
+    "https://images.pexels.com/photos/1440476/pexels-photo-1440476.jpeg?auto=compress&cs=tinysrgb&h=130",
+    { responseType: "arraybuffer" }
+  );
+
+  // S3.put(
+  //   "image.jpeg",
+  //   `${image.data}.jpeg`,
+  //   getEnv("IMAGES_BUCKET_NAME"),
+  //   "public-read"
+  // );
   //@ts-ignore
   try {
     //@ts-ignore
